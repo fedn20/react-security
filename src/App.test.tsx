c97bf17b566355244b95d3b3fe7b31cbd317f360
sbabureddy <sbabureddy01@gmail.com>
2021-10-02 14:25:31 +0530

    Fix test case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react security", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const securityElement = screen.getByRole("heading", {
+    name: /react security/i,
+  });
+  expect(securityElement).toBeInTheDocument();
 });
