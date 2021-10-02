58e8b41925abbf052f4e33a8990c2177338e944b
sbabureddy <sbabureddy01@gmail.com>
2021-10-02 15:10:19 +0530

    Updates

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}", "!src/reportWebVitals.ts"],
-  coveragePathIgnorePatterns: ["src/index.tsx", "src/reportWebVitals.ts"],
-  coverageReporters: ["json", "text", "html", "lcov"],
-};
